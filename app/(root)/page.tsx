8430415e7f2693005628a57fd139a0a5c9b8a51a
Abiraj k <abhi@Abirajs-MacBook-Air.local>
2024-02-27 17:43:23 +0530

    ui with sidebar and mobile resposnive

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <div className="p">Home</div>
+      {/* <UserButton afterSignOutUrl="/" /> */}
+    </div>
+  );
+}
+
+export default Home;
